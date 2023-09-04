5ba1202a00234a144284987cb3a9f96fc6a7ee05
gusgusz <gustavoguimaraescamps@gmail.com>
2023-09-04 12:55:08 -0300

    feat: HTTP complete CRUD  posts component

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,24 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PostsService } from './posts.service';
 import { LoadingService } from '../loading/loading.service';
 import { Observable } from 'rxjs';
-
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent {
- posts : any[] = [];
+export class PostsComponent implements OnInit {
+  posts: any[] = [];
+  newPost: any = {
+    title: '',
+    body: ''
+  };
 
- constructor(
-  private postsService: PostsService,
-  private loadingService: LoadingService,
-) {}
+  constructor(
+    private postsService: PostsService,
+    private loadingService: LoadingService
+  ) {}
 
-loading$!: Observable<boolean>;
-
+  loading$!: Observable<boolean>;
+  search: string = "";
+  step: number = 0; 
 
   ngOnInit() {
     this.getPosts();
@@ -26,12 +32,67 @@ loading$!: Observable<boolean>;
 
   getPosts() {
     if (this.posts.length === 0) {
-      this.loading$ = this.loadingService.loading$; 
-      this.loadingService.show(); 
+      this.loading$ = this.loadingService.loading$;
+      this.loadingService.show();
     }
     this.postsService.getPosts().subscribe((data: any) => {
       this.posts = data;
-      this.loadingService.hide(); 
+      this.loadingService.hide();
     });
+  }
+
+  searchPosts(search: string) {
+    if (search.length >= 3) {
+      this.loading$ = this.loadingService.loading$;
+      this.loadingService.show();
+      this.postsService.searchPostsTitle(search).subscribe((data: any) => {
+        this.posts = data;
+        this.loadingService.hide();
+      });
+    }
+  }
+
+  createPost(post: any) {
+    this.postsService.createPost(post).pipe(
+      catchError((error) => {
+        console.error('Ocorreu um erro ao criar a postagem:', error);
+        return throwError(() => error);
+      })
+    ).subscribe(() => {
+      this.posts.unshift(post);
+      this.newPost = { title: '', body: '' };
+    });
+  }
+
+  updatePost(postId: number, post: any) {
+    this.postsService.updatePost(postId, post).pipe(
+      catchError((error) => {
+        console.error('Ocorreu um erro ao atualizar a postagem:', error);
+        return throwError(() => error);
+      })
+    ).subscribe((data: any) => {
+      const updatedPosts = this.posts.map((p) => (p.id === postId ? data : p));
+      this.posts = updatedPosts;
+    });
+  }
+
+  deletePost(postId: number) {
+    this.postsService.deletePost(postId).pipe(
+      catchError((error) => {
+        console.error('Ocorreu um erro ao excluir a postagem:', error);
+        return throwError(() => error);
+      })
+    ).subscribe(() => {
+      this.posts = this.posts.filter((post) => post.id !== postId);
+    });
+  }
+
+  toggleEditMode(post: any) {
+    post.isEditing = !post.isEditing;
+  }
+
+  executeEditAndPut(post: any) {
+    this.toggleEditMode(post);
+    this.updatePost(post.id, post);
   }
 }
--- src/app/components/posts/posts.service.ts
+++ src/app/components/posts/posts.service.ts
@@ -1,16 +1,44 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class PostsService {
   constructor(private http: HttpClient) {}
 
   getPosts() {
-    const apiUrl = environment.apiUrl + '/posts'; 
+    const apiUrl = environment.apiUrl + '/posts';
     return this.http.get(apiUrl);
   }
-}
 
+  createPost(post: any): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer ' + environment.token,
+    });
+    return this.http.post(environment.apiUrl + '/posts', post, { headers });
+  }
+
+  updatePost(postId: number, post: any): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer ' + environment.token,
+    });
+    const url = `${environment.apiUrl + '/posts'}/${postId}`;
+    return this.http.put(url, post, { headers });
+  }
+
+  deletePost(postId: number): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer ' + environment.token,
+    });
+    const url = `${environment.apiUrl}/posts/${postId}`;
+    return this.http.delete(url, { headers });
+  }
+
+  searchPostsTitle(search: string){
+    const url = `${environment.apiUrl + '/posts?title='+ search}`;
+    return this.http.get(url);
+  }
+}
